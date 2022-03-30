c7d2656b1ba01d3019e7bdbc30f0c25f7f07b5a7
Sudarsh <55168611+Sudarsh1010@users.noreply.github.com>
2022-03-30 12:12:54 +0530

    exported images as constants

---
+++ src/constants/images.js
@@ -0,0 +1,117 @@
+// accessories
+import accEarings from '../assets/accessories/earings.png'
+import accflower from '../assets/accessories/flower.png'
+import accglasses from '../assets/accessories/glasses.png'
+import accheadphone from '../assets/accessories/headphone.png'
+
+// backgrounds
+import blue50 from '../assets/backgrounds/blue50.png'
+import blue60 from '../assets/backgrounds/blue60.png'
+import blue70 from '../assets/backgrounds/blue70.png'
+import darkblue30 from '../assets/backgrounds/darkblue30.png'
+import darkblue50 from '../assets/backgrounds/darkblue50.png'
+import darkblue70 from '../assets/backgrounds/darkblue70.png'
+import green50 from '../assets/backgrounds/green50.png'
+import green60 from '../assets/backgrounds/green60.png'
+import green70 from '../assets/backgrounds/green70.png'
+import grey40 from '../assets/backgrounds/grey40.png'
+import grey70 from '../assets/backgrounds/grey70.png'
+import grey80 from '../assets/backgrounds/grey80.png'
+import red50 from '../assets/backgrounds/red50.png'
+import red60 from '../assets/backgrounds/red60.png'
+import red70 from '../assets/backgrounds/red70.png'
+import yellow50 from '../assets/backgrounds/yellow50.png'
+import yellow60 from '../assets/backgrounds/yellow60.png'
+import yellow70 from '../assets/backgrounds/yellow70.png'
+
+// ears
+import defaultEars from '../assets/ears/default.png'
+import tiltBackwardEars from '../assets/ears/tilt-backward.png'
+import tiltForwardEars from '../assets/ears/tilt-forward.png'
+
+// eye
+import angryEye from '../assets/eyes/angry.png'
+import defaultEye from '../assets/eyes/default.png'
+import naughtyEye from '../assets/eyes/naughty.png'
+import pandaEye from '../assets/eyes/panda.png'
+import smartEye from '../assets/eyes/smart.png'
+import starEye from '../assets/eyes/star.png'
+
+// hair
+import bang from '../assets/hair/bang.png'
+import curls from '../assets/hair/curls.png'
+import elegant from '../assets/hair/elegant.png'
+import fancy from '../assets/hair/fancy.png'
+import quiff from '../assets/hair/quiff.png'
+import short from '../assets/hair/short.png'
+import defaultHair from '../assets/hair/default.png'
+
+// leg 
+import bubbleTea from '../assets/leg/bubble-tea.png'
+import cookie from '../assets/leg/cookie.png'
+import defaultLeg from '../assets/leg/default.png'
+import gameConsole from '../assets/leg/game-console.png'
+import tiltBackwardLeg from '../assets/leg/tilt-backward.png'
+import tiltForwardLeg from '../assets/leg/tilt-forward.png'
+
+// neck
+import defaultNeck from '../assets/neck/default.png'
+import bendForwardNeck from '../assets/neck/bend-forward.png'
+import bendBackwardNeck from '../assets/neck/bend-backward.png'
+import thick from '../assets/neck/thick.png'
+
+// nose
+import nose from '../assets/nose.png'
+
+// export 
+export default {
+  accEarings,
+  accflower,
+  accglasses,
+  accheadphone,
+  blue50,
+  blue60,
+  blue70,
+  darkblue30,
+  darkblue50,
+  darkblue70,
+  green50,
+  green60,
+  green70,
+  grey40,
+  grey70,
+  grey80,
+  red50,
+  red60,
+  red70,
+  yellow50,
+  yellow60,
+  yellow70,
+  defaultEars,
+  tiltBackwardEars,
+  tiltForwardEars,
+  angryEye,
+  defaultEye,
+  naughtyEye,
+  pandaEye,
+  smartEye,
+  starEye,
+  bang,
+  curls,
+  elegant,
+  fancy,
+  quiff,
+  short,
+  defaultHair,
+  bubbleTea,
+  cookie,
+  defaultLeg,
+  gameConsole,
+  tiltBackwardLeg,
+  tiltForwardLeg,
+  defaultNeck,
+  bendForwardNeck,
+  bendBackwardNeck,
+  thick,
+  nose,
+}
